1f158edc7133d97a17aa2d79ca29e1ab1e3ac8be
Klaus Borges <klausborgs@gmail.com>
2024-10-15 22:25:58 -0300

    refactor: clean up some minor things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [icon()],
 });
-
