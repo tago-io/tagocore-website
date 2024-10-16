fe1c452d0f17bf03c2f89653712da8b68e989d24
Klaus Borges <klausborgs@gmail.com>
2024-10-15 22:18:55 -0300

    refactor: full rewrite of TagoCore website in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  integrations: [icon()],
+});
+
